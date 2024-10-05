9f6b69ef6d09370ae189cd9de0cd85232db66ee5
chailana <chailana35217793@gmail.com>
2024-10-05 21:00:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start Gunicorn and log output
-gunicorn app:app --bind 0.0.0.0:8000 > gunicorn.log 2>&1 &
+# Start the HTTP server in the background on port 8000
+python3 http_server.py &
 
-# Start the bot and log output
-python3 bot.py > bot.log 2>&1
+# Start the Telegram bot
+python3 bot.py
